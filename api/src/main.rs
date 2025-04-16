0529449fc2e3db55ea46e0821a0537edd353d24d
Carexo <marcel.mietla3@gmail.com>
2025-04-16 12:33:19 +0200

    fix: docker compose

--- api/src/main.rs
+++ api/src/main.rs
@@ -15,9 +15,9 @@ fn index() -> &'static str {
 #[launch]
 async fn rocket() -> _ {
     dotenv().ok();
-    let neo4j_uri = env::var("NEO4J_URI").unwrap_or_else(|_| "bolt://neo4j:7687".to_string());
-    let neo4j_user = env::var("NEO4J_USER").unwrap_or_else(|_| "neo4j".to_string());
-    let neo4j_password = env::var("NEO4J_PASSWORD").expect("NEO4J_PASSWORD not set");
+    let neo4j_uri = env::var("DB_URI").unwrap_or_else(|_| "bolt://neo4j:7687".to_string());
+    let neo4j_user = env::var("DB_USER").unwrap_or_else(|_| "neo4j".to_string());
+    let neo4j_password = env::var("DB_PASSWORD").expect("NEO4J_PASSWORD not set");
 
     let neo4j = Neo4jConnection::new(
         &neo4j_uri,
