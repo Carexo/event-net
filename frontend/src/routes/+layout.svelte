34d5e456aa519d5f3091c063965a1bf7c2aab71c
Carexo <marcel.mietla3@gmail.com>
2025-05-17 23:21:24 +0200

    feat:  add basic pages and event card component

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,23 +1,26 @@
 <script lang="ts">
 	import '../app.css';
     import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
-    import { Footer, FooterCopyright, FooterLinkGroup, FooterLink } from "flowbite-svelte";
+    import { Footer, FooterCopyright, FooterLinkGroup, FooterLink, DarkMode} from "flowbite-svelte";
 
 	let { children } = $props();
 </script>
 
 <Navbar>
     <NavBrand href="/">
-<!--        <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />-->
+        <img src="/logo.png" class="me-3 h-9 sm:h-12" alt="Flowbite Logo" />
         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">event net</span>
     </NavBrand>
     <NavHamburger />
     <NavUl>
         <NavLi href="/">Home</NavLi>
+        <NavLi href="/users">Users</NavLi>
         <NavLi href="/events">All events</NavLi>
     </NavUl>
 </Navbar>
-{@render children()}
+<main class="p-5 lg:p-10">
+    {@render children()}
+</main>
 <Footer>
     <FooterCopyright href="/" by="Marceli Mietła & Bartosz Czyż™" year={2025} />
     <FooterLinkGroup class="mt-3 flex flex-wrap items-center text-sm text-gray-500 sm:mt-0 dark:text-gray-400">
--- frontend/src/routes/events/+page.svelte
+++ frontend/src/routes/events/+page.svelte
@@ -1,1 +1,13 @@
-<h1>all events</h1>
+<script lang="ts">
+    import {Heading} from "flowbite-svelte";
+    import EventCard from "$lib/components/EventCard.svelte";
+</script>
+
+<div class="text-center">
+    <Heading tag="h1" class="py-5">All events</Heading>
+    <section class="p-5 flex gap-8 flex-wrap justify-center">
+        <EventCard />
+        <EventCard />
+        <EventCard />
+    </section>
+</div>
