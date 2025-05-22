e80a0a14009eb4a8d5e357a5aaf9d243553998b0
Carexo <marcel.mietla3@gmail.com>
2025-05-22 23:20:37 +0200

    feat: add status to api response

--- api/src/utils/api_response.rs
+++ api/src/utils/api_response.rs
@@ -1,3 +1,4 @@
+use rocket::http::Status;
 use rocket::response::Responder;
 use rocket::serde::json::Json;
 use serde::Serialize;
@@ -5,27 +6,37 @@ use serde::Serialize;
 #[derive(Debug, Clone, Serialize)]
 #[serde(untagged)]
 pub enum ApiResponse<T> {
-    Success { data: T, message: String },
-    Error { message: String },
+    Success { data: T, message: String, #[serde(skip)] status: Status },
+    MessageOnly { message: String, #[serde(skip)] status: Status },
 }
 
 impl<T: Serialize> ApiResponse<T> {
     pub fn success(data: T, message: impl Into<String>) -> Self {
         Self::Success {
             data,
-            message: message.into()
+            message: message.into(),
+            status: Status::Ok
         }
     }
 
-    pub fn error(message: impl Into<String>) -> Self {
-        Self::Error {
+    pub fn message_only(message: impl Into<String>, status: Status) -> Self {
+        Self::MessageOnly {
             message: message.into(),
+            status
         }
     }
 }
 
 impl<'r, T: Serialize> Responder<'r, 'static> for ApiResponse<T> {
     fn respond_to(self, req: &'r rocket::Request<'_>) -> rocket::response::Result<'static> {
-        Json(self).respond_to(req)
+        let status = match &self {
+            Self::Success { status, .. } => *status,
+            Self::MessageOnly { status, .. } => *status,
+        };
+
+        let json_response = Json(self);
+        rocket::Response::build_from(json_response.respond_to(req)?)
+            .status(status)
+            .ok()
     }
 }
