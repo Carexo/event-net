53baf426bc202d5601082342d8c41cd0610a3540
Carexo <marcel.mietla3@gmail.com>
2025-05-25 10:02:42 +0200

    feat: add cors to api and feature events endpoint

--- api/src/routes/events.rs
+++ api/src/routes/events.rs
@@ -5,26 +5,36 @@ use std::sync::Arc;
 
 use crate::repo::events::EventRepository;
 use crate::services::events::EventService;
-use crate::utils::api_response::ApiResponse;
-use rocket::serde::{json::Json};
 use crate::services::users_events::UserEventService;
+use crate::utils::api_response::ApiResponse;
+use rocket::serde::json::Json;
 
 pub struct EventController {
     event_service: Arc<EventService>,
-    user_event_service: Arc<UserEventService>
+    user_event_service: Arc<UserEventService>,
 }
 
 impl EventController {
     pub fn new(
         event_service: Arc<EventService>,
-        user_event_service: Arc<UserEventService>
+        user_event_service: Arc<UserEventService>,
     ) -> Self {
-        Self { event_service, user_event_service }
+        Self {
+            event_service,
+            user_event_service,
+        }
     }
 
     pub fn routes() -> Vec<rocket::Route> {
-        routes![get_all, get_one, add, delete, edit,
-            assign_user_to_event, unassign_user_from_event
+        routes![
+            get_all,
+            get_one,
+            add,
+            delete,
+            edit,
+            assign_user_to_event,
+            unassign_user_from_event,
+            get_featured
         ]
     }
 }
@@ -50,24 +60,42 @@ async fn delete(controller: &State<EventController>, id: u16) -> ApiResponse<Str
 }
 
 #[put("/event/<id>", format = "application/json", data = "<event>")]
-async fn edit(controller: &State<EventController>, id: u16, event: Json<EventUpdate>) -> ApiResponse<Event> {
-    controller.event_service.edit_event(id, event.into_inner()).await
+async fn edit(
+    controller: &State<EventController>,
+    id: u16,
+    event: Json<EventUpdate>,
+) -> ApiResponse<Event> {
+    controller
+        .event_service
+        .edit_event(id, event.into_inner())
+        .await
 }
 
 #[put("/events/<event_id>/attendees/<user_name>")]
 async fn assign_user_to_event(
     controller: &State<EventController>,
     event_id: u16,
-    user_name: &str
+    user_name: &str,
 ) -> ApiResponse<String> {
-    controller.user_event_service.assign_user_to_event(user_name, event_id).await
+    controller
+        .user_event_service
+        .assign_user_to_event(user_name, event_id)
+        .await
 }
 
 #[delete("/events/<event_id>/attendees/<user_name>")]
 async fn unassign_user_from_event(
     controller: &State<EventController>,
     event_id: u16,
-    user_name: &str
+    user_name: &str,
 ) -> ApiResponse<String> {
-    controller.user_event_service.unassign_user_from_event(user_name, event_id).await
+    controller
+        .user_event_service
+        .unassign_user_from_event(user_name, event_id)
+        .await
+}
+
+#[get("/events/featured")]
+async fn get_featured(controller: &State<EventController>) -> ApiResponse<Vec<Event>> {
+    controller.event_service.get_featured_events().await
 }
