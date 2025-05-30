1639209e70a5c09f3c8094d62737aa67b8b3cbc7
Bartosz Czyż <czyzbartosz3@gmail.com>
2025-05-30 18:21:42 +0200

    feat: add keyword filter on frontend

--- frontend/src/routes/events/+page.svelte
+++ frontend/src/routes/events/+page.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
     import type {PageProps} from './$types';
-    import {Heading} from "flowbite-svelte";
+    import {Heading, MultiSelect, Button} from "flowbite-svelte";
     import EventCard from "$lib/components/EventCard.svelte";
-
+    import {goto} from "$app/navigation";
     let {data}: PageProps = $props();
+
+    const keywords = data.keywords?.map((item) => ({ value: item, name: item }));
+    const multiSelectPlaceholder = "keywords";
+    let selected: string[] = $state(data.keywordsParams || []);
+
+    const getSearchParams = () => {
+        console.log(selected);
+        const searchParams = new URLSearchParams();
+        selected.forEach(kw => searchParams.append("keyword", kw));
+        goto(new URL(window.location.href).pathname + "?" + searchParams.toString());
+    }
 </script>
 
 <div class="text-center">
     <Heading tag="h1" class="py-5">All events</Heading>
+    <div class="flex gap-8 place-self-center" >
+        <MultiSelect class="w-lg" items={keywords} bind:value={selected} size="lg" placeholder={multiSelectPlaceholder} />
+        <Button class="cursor-pointer" onclick={() => getSearchParams()} outline color="light" size="xl">Search</Button>
+    </div>
 
     {#if data.error}
         <div class="text-red-500 p-8">
